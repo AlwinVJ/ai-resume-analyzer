a209ea5e9c10d3228391884e4bf53acd1f896afa
AlwinVJ <alwinvj5@gmail.com>
2025-07-26 10:23:37 +0530

    Analysis[feeback, ats score and tips] and retriving analysis completed

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -61,6 +61,7 @@ const Upload = () => {
         await kv.set(`resume:${uuid}`, JSON.stringify(data));
         setStatusText('Analysis Completed, redirecting..');
         console.log(data);
+        navigate(`/resume/${uuid}`);
 
     }
 
